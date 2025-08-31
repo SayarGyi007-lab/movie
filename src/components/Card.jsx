5b251b5d9e9bc7571efb344a88194603ef92e4f8
SayarGyi007-lab <your-thss152021@gmail.com>
2025-08-31 14:05:25 +0700

    final

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,31 @@ import React from 'react'
 import { PlayIcon } from "@heroicons/react/24/solid"
 import { Link } from 'react-router-dom'
 
-function Card({movie}) {
-    return (
-        <Link className='rounded-md overflow-hidden relative group' to={`/details/${movie.id}`}>
-            <div>
-                <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} 
-                    alt={movie.poster_path}
-                    className='w-full object-cover' />
-            </div>
-            <div className='dark-ov'></div>
+function Card({ movie }) {
+  return (
+    <Link 
+      className="rounded-md overflow-hidden relative group block" 
+      to={`/details/${movie.id}`}
+    >
+      {/* Poster */}
+      <div className="overflow-hidden">
+        <img 
+          src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} 
+          alt={movie.title || 'poster'} 
+          className="w-full object-cover transition-transform duration-500 group-hover:scale-110" 
+        />
+      </div>
 
-            <PlayIcon className='play-ic group-hover:top-1/2 group-hover:bottom-auto' />
-        </Link>
-    )
+      {/* Dark overlay */}
+      <div className="absolute inset-0 bg-black/30"></div>
+
+      {/* Play Icon always centered */}
+      <PlayIcon 
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-12 h-12 text-red-500 group-hover:transition-opacity duration-300"
+      />
+    </Link>
+  )
 }
 
 export default Card
